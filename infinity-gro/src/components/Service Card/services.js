ee10ee8b9d0d670888f9effca2a1bb036cb665f3
avi <avi.contact22@gmail.com>
2024-02-22 16:22:49 +0530

    Added information json

---
+++ infinity-gro/src/components/Service Card/services.js
@@ -0,0 +1,32 @@
+export const services = [
+  {
+    id: 1,
+    title: "Apostille",
+    description:
+      " A specialized certificate from the Secretary of State, validates documents like birth certificates universally under Hague Convention standards. Your key to global acceptance, ensuring legality and credibility worldwide",
+  },
+  {
+    id: 2,
+    title: "Attestation",
+    description:
+      "Performed by CPAs, evaluates a company's financial statement reliability. Guided by AICPA, it assures professional standards adherence, ensuring credibility and trust in financial reporting.",
+  },
+  {
+    id: 3,
+    title: "Legalization",
+    description:
+      "Legalization makes documents globally valid, removing legal barriers. It confirms official authority for marriage, birth, PCC, and commercial certificates. Delegate the process for easy international acceptance, ensuring cross-border recognition hassle-free.",
+  },
+  {
+    id: 4,
+    title: "Embassy Attestation",
+    description:
+      "Embassy attestation is a vital, complex procedure ensuring document security and authenticity against forgery. Obtain attestation stamps from your country's embassy, confirming the legitimacy of credentials like diplomas and certificates",
+  },
+  {
+    id: 5,
+    title: "HRD Attestation",
+    description:
+      "HRD Attestation validates educational documents like mark sheets, SSC, and degree certificates, affirming their authenticity by the respective university. Crucial for international mobility, ensuring credibility when applying for education or employment abroad.",
+  },
+];
